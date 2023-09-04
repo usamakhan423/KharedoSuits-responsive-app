881de7b13864c4cfd5fd5230e6a521d5035e943e
Usama Khan <uk352351@gmail.com>
2023-09-04 06:24:00 -0700

    Configuration done

--- src/components/Header.js
+++ src/components/Header.js
@@ -17,7 +17,7 @@ function Header() {
       <div className="container relative z-10 mx-auto text-center">
         <h1 className="text-4xl font-extrabold text-white mb-4">Discover the Perfect Kids' Suits</h1>
         <p className="text-lg text-white mb-8">Explore our premium collection of seasonal suits for kids. Dress your little ones in style for any occasion.</p>
-        <a href="#" className="bg-yellow-500 hover:bg-yellow-600 text-white text-lg font-semibold py-3 px-8 rounded-full inline-block transition duration-300 transform hover:scale-105 ease-in-out">Buy Now</a>
+        <a href="/" className="bg-yellow-500 hover:bg-yellow-600 text-white text-lg font-semibold py-3 px-8 rounded-full inline-block transition duration-300 transform hover:scale-105 ease-in-out">Buy Now</a>
       </div>
     </header>
   );
--- src/data.js
+++ src/data.js
@@ -1,4 +1,4 @@
-import background from './assets/background.jpg';
+
 import img1 from './assets/img1.jpg';
 import img2 from './assets/img2.jpg';
 import img3 from './assets/img3.jpg';
